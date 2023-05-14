d07135fd37db68446bd7863d81fb4003a11cd761
Bodrul Choudhury <Bodrul.Choudhury@StoneX.com>
2023-05-14 14:47:47 +0100

    Improved code by using strategies.

--- NasaRover/Models/Rover.cs
+++ NasaRover/Models/Rover.cs
@@ -1,105 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
 using NasaRover.Enums;
 
 namespace NasaRover.Models
 {
     public class Rover
     {
-        /// <summary>
-        /// Current position of the rover.
-        /// </summary>
-        public Position CurrentPosition { get; set; }
+        public Position CurrentPosition { get; }
 
-        /// <summary>
-        /// Actions the rover according to the given command.
-        /// </summary>
-        /// <param name="commands">One or more commands.</param>
+        private static readonly Dictionary<Command, Action<Rover>> CommandStrategies = new Dictionary<Command, Action<Rover>>
+        {
+            { Command.Move, rover => rover.Move() },
+            { Command.Right, rover => rover.Turn(Command.Right) },
+            { Command.Left, rover => rover.Turn(Command.Left) }
+        };
+
+        private static readonly Dictionary<(Direction, Command), Direction> DirectionStrategies = new Dictionary<(Direction, Command), Direction>
+        {
+            { (Direction.North, Command.Left), Direction.West },
+            { (Direction.North, Command.Right), Direction.East },
+            { (Direction.East, Command.Left), Direction.North },
+            { (Direction.East, Command.Right), Direction.South },
+            { (Direction.South, Command.Left), Direction.East },
+            { (Direction.South, Command.Right), Direction.West },
+            { (Direction.West, Command.Left), Direction.South },
+            { (Direction.West, Command.Right), Direction.North }
+        };
+
+        private static readonly Dictionary<Direction, Action<Position>> MoveStrategies = new Dictionary<Direction, Action<Position>>
+        {
+            { Direction.North, position => position.Y++ },
+            { Direction.East, position => position.X++ },
+            { Direction.South, position => position.Y-- },
+            { Direction.West, position => position.X-- }
+        };
+
+        public Rover(Position initialPosition) => CurrentPosition = initialPosition ?? throw new ArgumentNullException(nameof(initialPosition));
+
         public void ExecuteCommands(params Command[] commands)
         {
-            foreach (Command command in commands)
+            if (commands is null) throw new ArgumentNullException(nameof(commands));
+
+            foreach (var command in commands)
             {
-                switch (command)
-                {
-                    case Command.Move:
-                        Move();
-                        break;
+                if (!CommandStrategies.TryGetValue(command, out var commandStrategy))
+                    throw new ArgumentException($"Invalid command: {command}", nameof(commands));
 
-                    case Command.Left:
-                        Turn(Command.Left);
-                        break;
-
-                    case Command.Right:
-                        Turn(Command.Right);
-                        break;
-
-                    default:
-                        throw new ArgumentOutOfRangeException(nameof(commands));
-                }
+                commandStrategy(this);
             }
         }
 
-        /// <summary>
-        /// Evaluates direction, then turns rover relatively from it's current position either left or right.
-        /// </summary>
         private void Turn(Command turnCommand)
         {
-            switch (CurrentPosition.Direction)
-            {
-                case Direction.North:
-                    if (turnCommand == Command.Left) CurrentPosition.Direction = Direction.West;
-                    else if (turnCommand == Command.Right) CurrentPosition.Direction = Direction.East;
-                    else throw new ArgumentOutOfRangeException(nameof(turnCommand));
-                    break;
+            if (!DirectionStrategies.TryGetValue((CurrentPosition.Direction, turnCommand), out var newDirection))
+                throw new ArgumentException($"Invalid turn command: {turnCommand} for current direction: {CurrentPosition.Direction}", nameof(turnCommand));
 
-                case Direction.East:
-                    if (turnCommand == Command.Left) CurrentPosition.Direction = Direction.North;
-                    else if (turnCommand == Command.Right) CurrentPosition.Direction = Direction.South;
-                    else throw new ArgumentOutOfRangeException(nameof(turnCommand));
-                    break;
-
-                case Direction.South:
-                    if (turnCommand == Command.Left) CurrentPosition.Direction = Direction.East;
-                    else if (turnCommand == Command.Right) CurrentPosition.Direction = Direction.West;
-                    else throw new ArgumentOutOfRangeException(nameof(turnCommand));
-                    break;
-
-                case Direction.West:
-                    if (turnCommand == Command.Left) CurrentPosition.Direction = Direction.South;
-                    else if (turnCommand == Command.Right) CurrentPosition.Direction = Direction.North;
-                    else throw new ArgumentOutOfRangeException(nameof(turnCommand));
-                    break;
-
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(CurrentPosition.Direction));
-            }
+            CurrentPosition.Direction = newDirection;
         }
 
-        /// <summary>
-        /// Evaluates the rover's direction, then moves rover accordingly.
-        /// </summary>
         private void Move()
         {
-            switch (CurrentPosition.Direction)
-            {
-                case Direction.North:
-                    CurrentPosition.Y++;
-                    break;
+            if (!MoveStrategies.TryGetValue(CurrentPosition.Direction, out var moveStrategy))
+                throw new ArgumentException($"Invalid move command: {CurrentPosition.Direction}", nameof(CurrentPosition.Direction));
 
-                case Direction.East:
-                    CurrentPosition.X++;
-                    break;
-
-                case Direction.South:
-                    CurrentPosition.Y--;
-                    break;
-
-                case Direction.West:
-                    CurrentPosition.X--;
-                    break;
-
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(CurrentPosition.Direction));
-            }
+            moveStrategy(CurrentPosition);
         }
     }
 }
--- NasaRover/Program.cs
+++ NasaRover/Program.cs
@@ -8,31 +8,24 @@ namespace NasaRover
     {
         private static void Main()
         {
-            // Instantiate the rover.
-            Rover rover = new Rover
+            var rover = new Rover(new Position
             {
-                // Set the rover's initial position.
-                CurrentPosition = new Position
-                {
-                    X = 1,
-                    Y = 2,
-                    Direction = Direction.North
-                }
-            };
+                X = 1,
+                Y = 2,
+                Direction = Direction.North
+            });
 
-            // Display the current position of the rover.
             Console.WriteLine($"Current position:{Environment.NewLine}{rover.CurrentPosition}");
 
-            // Send commands to the rover.
             rover.ExecuteCommands(
                 Command.Left, Command.Move,
                 Command.Left, Command.Move,
                 Command.Left, Command.Move,
                 Command.Left, Command.Move,
+                Command.Move, Command.Right,
                 Command.Move
                 );
 
-            // Display the new position of the rover.
             Console.WriteLine($"New position:{Environment.NewLine}{rover.CurrentPosition}");
             Console.Read();
         }
